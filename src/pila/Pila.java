8c928b2859df6ad481cb403724800132856bdb9c
jaime <jvidalgz@gmail.com>
2015-04-08 14:14:52 -0300

    agrega metodos

--- src/pila/Pila.java
+++ src/pila/Pila.java
@@ -3,5 +3,29 @@ package pila;
 /**
  * Created by jaime on 08-04-2015.
  */
-public class Pila {
+public class Pila<T> {
+    private Nodo<T> cima;
+
+    public Pila() {
+        cima = null;
+    }
+
+    public void apilar(T nuevo) {
+
+        Nodo<T> nuevoElem = new Nodo<T>(nuevo);
+        Nodo<T> aux;
+
+        if (cima == null) {
+            cima = nuevoElem;
+        } else {
+            aux = cima;
+            cima = nuevoElem;
+            cima.setEnlace(aux);
+        }
+    }
+
+    public Nodo verTop() {
+        return cima;
+    }
+
 }
