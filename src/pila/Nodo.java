eda126bb44fbab54c56d66c86b2767b74593ebb4
jaime <jvidalgz@gmail.com>
2015-04-08 13:45:16 -0300

    agrega constructor mejorado

--- src/pila/Nodo.java
+++ src/pila/Nodo.java
@@ -1,12 +1,18 @@
 package pila;
 
+import persona.Persona;
+
 /**
  * Created by jaime on 08-04-2015.
  */
 public class Nodo {
 
+    private Persona dato;
+    private Persona enlace;
+
     //constructor
-    Nodo() {
-        //hola
+    Nodo(Persona nueva) {
+        this.dato = nueva;
+        this.enlace = null;
     }
 }
