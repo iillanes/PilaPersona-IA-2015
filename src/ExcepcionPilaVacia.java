5e121f3b3e860782d3229e62076e3996c6316030
jaime <jvidalgz@gmail.com>
2015-04-08 14:36:04 -0300

    crea ExcepcionPilaVacia

---
+++ src/ExcepcionPilaVacia.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jaime on 08-04-2015.
+ */
+public class ExcepcionPilaVacia extends Exception {
+
+    public ExcepcionPilaVacia() {
+        super("La Pila está vacía, no se puede desapilar más");
+    }
+
+}
